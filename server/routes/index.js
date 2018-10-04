8726fb6ecada4dec5742d02d4b4cb97aafc7904b
Jacob McSwain <jacob@clevyr.com>
2018-10-03 23:05:40 -0500

    Add the ability to add transaction types

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,6 @@ const transactions = require('./transactions');
 const bank_accounts = require('./bank_accounts');
 const debts = require('./debt');
 const groceries = require('./groceries')
+const transaction_types = require('./transaction_type')
 
-module.exports = [].concat(auth, user, transactions, bank_accounts, debts, groceries);
+module.exports = [].concat(auth, user, transactions, bank_accounts, debts, groceries, transaction_types);
