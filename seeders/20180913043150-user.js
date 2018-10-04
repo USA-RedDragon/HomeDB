0ff112591370bd8d3b6841972f2a0c664c35bac7
Jacob McSwain <jacob@clevyr.com>
2018-10-03 23:05:13 -0500

    Remove unneeded seeds

--- seeders/20180913043150-user.js
+++ seeders/20180913043150-user.js
@@ -12,48 +12,6 @@ module.exports = {
       createdAt: '2018-09-11 04:31:36',
       updatedAt: '2018-09-11 04:31:36'
     }], {});
-
-    await queryInterface.bulkInsert('transaction_types', [{
-      name: 'Groceries',
-      createdAt: '2018-09-11 04:31:36',
-      updatedAt: '2018-09-11 04:31:36'
-    }], {});
-
-    await queryInterface.bulkInsert('default_groceries', [{
-      grocery: 1,
-      amount: 1,
-      createdAt: '2018-09-11 04:31:36',
-      updatedAt: '2018-09-11 04:31:36'
-    },
-    {
-      grocery: 2,
-      amount: 5,
-      createdAt: '2018-09-11 04:31:36',
-      updatedAt: '2018-09-11 04:31:36'
-    }], {});
-
-    return queryInterface.bulkInsert('groceries', [
-    {
-      name: 'Milk',
-      createdAt: '2018-09-11 04:31:36',
-      updatedAt: '2018-09-11 04:31:36'
-    },
-    {
-      name: 'Eggs',
-      createdAt: '2018-09-11 04:31:36',
-      updatedAt: '2018-09-11 04:31:36'
-    },
-    {
-      name: 'Ground Beef',
-      createdAt: '2018-09-11 04:31:36',
-      updatedAt: '2018-09-11 04:31:36'
-    },
-    {
-      name: 'Butter',
-      createdAt: '2018-09-11 04:31:36',
-      updatedAt: '2018-09-11 04:31:36'
-    }
-  ], {});
   },
 
   down: (queryInterface, Sequelize) => {
