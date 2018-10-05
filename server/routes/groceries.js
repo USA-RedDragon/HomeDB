e57c0272dcca807617ac6cf595a5daf8997b586b
Jacob McSwain <jacob@clevyr.com>
2018-10-05 11:43:32 -0500

    Add more grocery utilities

--- server/routes/groceries.js
+++ server/routes/groceries.js
@@ -10,20 +10,35 @@ module.exports = [
         handler: async (request, h) => {
             const Groceries = request.getModel('groceries');
             const GroceryList = request.getModel('grocery_list');
-            var groceries = await GroceryList.findAll({});
+            var groceries = await GroceryList.findAll({
+                include: [{
+                    model: Groceries,
+                    required: true,
+                    as: "grocery"
+                }]
+            });
 
-            var nameAdded = [];
-            for (var i = 0, len = groceries.length; i < len; i++) {
-                var grocery = await Groceries.findOne({ where: { id: groceries[i].grocery } });
-                nameAdded.push(groceries[i].toJSON());
-                nameAdded[i].name = grocery.name;
-            }
-
-            return nameAdded;
+            return groceries;
         }
     },
     {
         method: 'GET',
+        path: '/api/default_groceries',
+        handler: async (request, h) => {
+            const Groceries = request.getModel('groceries');
+            const DefaultGroceries = request.getModel('default_groceries');
+            var groceries = await DefaultGroceries.findAll({
+                include: [{
+                    model: Groceries,
+                    required: true,
+                    as: "grocery"
+                }]
+            });
+            return groceries;
+        }
+    },
+    {
+        method: 'POST',
         path: '/api/generate_groceries',
         handler: async (request, h) => {
             const DefaultGroceries = request.getModel('default_groceries');
@@ -31,30 +46,31 @@ module.exports = [
             const Groceries = request.getModel('groceries');
             var defaultGroceries = await DefaultGroceries.findAll({});
 
-            await request.getModel('grocery_list').destroy({
+            await GroceryList.destroy({
                 where: {},
                 truncate: true
             });
 
             for (var i = 0, len = defaultGroceries.length; i < len; i++) {
                 await GroceryList.create({
-                    grocery: defaultGroceries[i].grocery,
+                    groceryId: defaultGroceries[i].groceryId,
                     amount: defaultGroceries[i].amount
                 });
             }
 
-            var grocery_list = await GroceryList.findAll({});
-            var new_groceries = [];
-            for (var i = 0, len = grocery_list.length; i < len; i++) {
-                var grocery = await Groceries.findOne({ where: { id: grocery_list[i].grocery } });
-                new_groceries.push(grocery_list[i].toJSON());
-                new_groceries[i].name = grocery.name;
-            }
-            return new_groceries;
+            var grocery_list = await GroceryList.findAll({
+                include: [{
+                    model: Groceries,
+                    required: true,
+                    as: "grocery"
+                }]
+            });
+            
+            return grocery_list;
         }
     },
     {
-        method: 'GET',
+        method: 'DELETE',
         path: '/api/clear_groceries',
         handler: async (request, h) => {
             await request.getModel('grocery_list').destroy({
@@ -66,6 +82,52 @@ module.exports = [
         }
     },
     {
+        method: 'DELETE',
+        path: '/api/clear_default_groceries',
+        handler: async (request, h) => {
+            await request.getModel('default_groceries').destroy({
+                where: {},
+                truncate: true
+            });
+
+            return h.response().code(204);
+        }
+    },
+    {
+        method: 'POST',
+        path: '/api/default_grocery_list',
+        handler: async (request, h) => {
+            const Groceries = request.getModel('groceries');
+            const DefaultGroceryList = request.getModel('default_groceries');
+
+            await DefaultGroceryList.create({
+                groceryId: request.payload.id,
+                amount: request.payload.amount
+            });
+
+            var default_groceries = await DefaultGroceryList.findAll({
+                include: [{
+                    model: Groceries,
+                    required: true,
+                    as: 'grocery'
+                }]
+            });
+
+            return default_groceries;
+        },
+        options: {
+            validate: {
+              options: {
+                stripUnknown: true
+              },
+              payload: {
+                id: Joi.number().required(),
+                amount: Joi.number().required()
+              }
+            },
+        }
+    },
+    {
         method: 'POST',
         path: '/api/grocery_list',
         handler: async (request, h) => {
@@ -73,19 +135,19 @@ module.exports = [
             const GroceryList = request.getModel('grocery_list');
                         
             await GroceryList.create({
-                grocery: request.payload.id,
+                groceryId: request.payload.id,
                 amount: request.payload.amount
             });
 
-            var groceries = await GroceryList.findAll({});
-            var nameAdded = [];
-            for (var i = 0, len = groceries.length; i < len; i++) {
-                var grocery = await Groceries.findOne({ where: { id: groceries[i].grocery } });
-                nameAdded.push(groceries[i].toJSON());
-                nameAdded[i].name = grocery.name;
-            }
+            var groceries = await GroceryList.findAll({
+                include: [{
+                    model: Groceries,
+                    required: true,
+                    as: 'grocery'
+                }]
+            });
 
-            return nameAdded;
+            return groceries;
         },
         options: {
             validate: {
@@ -107,6 +169,28 @@ module.exports = [
             var groceries = await Groceries.findAll({});
 
             return groceries;
+        }
+    },
+    {
+        method: 'POST',
+        path: '/api/groceries',
+        handler: async (request, h) => {
+            const Groceries = request.getModel('groceries');
+                        
+            var grocery = await Groceries.create({
+                name: request.payload.name,
+            });
+            return grocery;
+        },
+        options: {
+            validate: {
+              options: {
+                stripUnknown: true
+              },
+              payload: {
+                name: Joi.string().required()
+              }
+            },
         }
     }
 ];
