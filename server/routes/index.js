dac3edbf739d83aca780265a1d61f72354c08e01
Jacob McSwain <jacob@clevyr.com>
2018-11-02 19:59:32 -0500

    Add paycheck support

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,8 @@ const user = require('./user');
 const transactions = require('./transactions');
 const bank_accounts = require('./bank_accounts');
 const debts = require('./debt');
+const paychecks = require('./paycheck');
 const groceries = require('./groceries')
 const transaction_types = require('./transaction_type')
 
-module.exports = [].concat(auth, user, transactions, bank_accounts, debts, groceries, transaction_types);
+module.exports = [].concat(auth, user, transactions, bank_accounts, debts, paychecks, groceries, transaction_types);
